77f0f09e9c66417504c9f095c83e19dacb96dc61
adenast <adenast@proton.me>
2026-01-03 00:21:56 +0500

    build: Add flake.nix for NixOS ecosystem compatibility

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "CLI utility for viewing system information, but with a twist";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "footfetch";
+          version = "1.4.0";
+          src = ./.;
+          
+          cargoHash = ""; 
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          meta = with pkgs.lib; {
+            description = "CLI utility for viewing system information, but with a twist";
+            homepage = "https://github.com/adenast/footfetch";
+            license = licenses.mit;
+            maintainers = [ "adenast" ];
+          };
+        };
+      }
+    );
+}
